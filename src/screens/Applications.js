27dff4aa999de1687ef30b91b0bd5633011744fb
Carlo <carlo.cajucom@gmail.com>
2017-03-27 14:49:45 -0700

    (Applications) add FlatList

--- src/screens/Applications.js
+++ src/screens/Applications.js
@@ -1,19 +1,24 @@
 // @flow
 
 import React, { Component } from 'react'
-import {
-  AppRegistry,
-  AsyncStorage,
-  ListView,
-  Linking,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
-import queryString from 'query-string'
-import base64 from 'base-64'
+const mockApplication = {
+  accessKeys: [
+    {
+      key: 'ttn-account-v2.8mJnERF9JBFJYAXO7HBMZd5UOIuuvb0CX7-hCpJ2KSE',
+      name: 'default key',
+      rights: ['messages:up:r', 'messages:down:w', 'devices'],
+    },
+  ],
+  created: '2017-01-22T04:23:30.208Z',
+  euis: ['70B3D57EF0003448'],
+  handler: 'ttn-handler-us-west',
+  id: '7581206457',
+  name: 'Test',
+}
+
+const mockApplications = [mockApplication]
 
 import { LATO_REGULAR } from '../constants/fonts'
 
@@ -22,6 +27,10 @@ export default class TTNConsole extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Test</Text>
+        <FlatList
+          data={mockApplications}
+          renderItem={application => <Text>{application.name}</Text>}
+        />
       </View>
     )
   }
