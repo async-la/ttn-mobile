43bb5f3c5ea80bf0259e6bbe6c4f0440f5a5ba28
Carlo <carlo.cajucom@gmail.com>
2017-03-29 15:15:09 -0700

    change tab label constants to formatted message

--- src/scopes/navigation/constants.js
+++ src/scopes/navigation/constants.js
@@ -1,5 +1,8 @@
 //@flow
 
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
 // route names
 export const APPLICATIONS = 'Applications'
 export const GATEWAYS = 'Gateways'
@@ -18,11 +21,27 @@ export const SETTINGS = 'Settings'
 export const TRAFFIC = 'Traffic'
 
 // tab labels
-export const APPLICATIONS_LABEL = 'Applications'
-export const DATA_LABEL = 'Data'
-export const DEVICES_LABEL = 'Devices'
-export const GATEWAYS_LABEL = 'Gateways'
-export const OVERVIEW_LABEL = 'Overview'
-export const PROFILE_LABEL = 'Profile'
-export const SETTINGS_LABEL = 'Settings'
-export const TRAFFIC_LABEL = 'Traffic'
+export const APPLICATIONS_LABEL = (
+  <FormattedMessage id="app.nav.applications" defaultMessage="Applications" />
+)
+export const DATA_LABEL = (
+  <FormattedMessage id="app.nav.data" defaultMessage="Data" />
+)
+export const DEVICES_LABEL = (
+  <FormattedMessage id="app.nav.devices" defaultMessage="Devices" />
+)
+export const GATEWAYS_LABEL = (
+  <FormattedMessage id="app.nav.gateways" defaultMessage="Gateways" />
+)
+export const OVERVIEW_LABEL = (
+  <FormattedMessage id="app.nav.overview" defaultMessage="Overview" />
+)
+export const PROFILE_LABEL = (
+  <FormattedMessage id="app.nav.profile" defaultMessage="Profile" />
+)
+export const SETTINGS_LABEL = (
+  <FormattedMessage id="app.nav.settings" defaultMessage="Settings" />
+)
+export const TRAFFIC_LABEL = (
+  <FormattedMessage id="app.nav.traffic" defaultMessage="Traffic" />
+)
