82d8eada7fc7ca5d02786f643994a2f8739bea83
Christopher Dro <hello@blick-labs.com>
2017-03-30 12:16:17 -0700

    [Auth] Add actions for get/refresh token. - Renders splash or root nav based on local auth state. - Adds action to get initial oauth token + refresh action (still requires implementation with apiClient)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 // @flow
-
 import React, { Component } from 'react'
+
+import AppNavigator from './scopes/navigation/navigator'
+import SplashHome from './screens/SplashHome'
+
 import { connect } from 'react-redux'
 import { addNavigationHelpers } from 'react-navigation'
 
-import AppNavigator from './scopes/navigation/navigator'
+type Props = {
+  auth: Object,
+  dispatch: Function,
+  nav: Object,
+};
 
 class App extends Component {
+  props: Props;
+  renderApp = () => {
+    const { auth } = this.props
+    if (auth.accessToken) {
+      return (
+        <AppNavigator
+          navigation={addNavigationHelpers({
+            dispatch: this.props.dispatch,
+            state: this.props.nav,
+          })}
+        />
+      )
+    } else {
+      return <SplashHome />
+    }
+  };
   render() {
-    return (
-      <AppNavigator
-        navigation={addNavigationHelpers({
-          dispatch: this.props.dispatch,
-          state: this.props.nav,
-        })}
-      />
-    )
+    return this.renderApp()
   }
 }
 
 export default connect(state => ({
+  auth: state.auth,
   nav: state.navigator,
 }))(App)
--- src/Root.js
+++ src/Root.js
@@ -4,12 +4,11 @@
 global.Intl = require('react-intl')
 
 import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import { AppRegistry } from 'react-native'
 
 import App from './App'
 import DelayUntilBootstrapped from './components/DelayUntilBootstrapped'
 import DeviceMonitor from './components/DeviceMonitor'
-import SplashHome from './screens/SplashHome'
 import { Provider } from 'react-redux'
 
 import configureStore from './store/configureStore'
@@ -27,7 +26,6 @@ export default class TTNConsole extends Component {
           <LanguageProvider translations={translations}>
             <DelayUntilBootstrapped>
               <App />
-              {/* <SplashHome /> */}
             </DelayUntilBootstrapped>
           </LanguageProvider>
         </DeviceMonitor>
