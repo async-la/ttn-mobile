18da0093ff7f17cc69fb0e552602f8c6ed527203
Daniel Merrill <daniel.r.merrill@gmail.com>
2017-04-10 16:04:33 -0700

    [colors] Add grey, red, green colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,10 +1,14 @@
 // @flow
 
 // Brand
-export const BLUE = '#0D83D0'
-export const DARK_GREY = '#363636'
 export const LIGHT_BLUE = '#D4EAF9'
+export const BLUE = '#0D83D0'
+export const LIGHT_RED = '#fff8f5'
+export const RED = '#fb510a'
+export const GREEN = '#71d7c3'
 export const LIGHT_GREY = '#F7F7F7'
+export const GREY = '#EEEEEE'
+export const DARK_GREY = '#363636'
 export const LIGHT_ORANGE = '#FFEECD'
 export const ORANGE = '#FFCB67'
 export const DARK_ORANGE = '#CD8700'
