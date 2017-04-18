9824ab027969eef9c3dafdf9ec0b3a311a206c1a
Zack Story <zack@root-two.com>
2017-04-18 15:06:26 -0700

    [tooling] sentry improvements (#80)
    * [sentry] localize script, commit configureSentry
    
    * [sentry] attempt ci fix with yarn run sentry-cli

---
+++ src/utils/configureSentry.js
@@ -0,0 +1,9 @@
+// @flow
+
+import { Sentry } from 'react-native-sentry/lib/Sentry'
+
+export default function() {
+  Sentry.config(
+    'https://c301d1166c6d4a5ba1984df20f5a2160:e81d375ff884466ca40f58da0134336e@sentry.io/159098'
+  ).install()
+}
