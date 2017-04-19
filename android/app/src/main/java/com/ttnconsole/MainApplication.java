e1ef89fdfe50b643c819a7f12f1dd9a8ec7a1c40
rt2zz <zack@root-two.com>
2017-04-18 23:01:57 -0700

    [tooling] remove sentry

--- android/app/src/main/java/com/ttnconsole/MainApplication.java
+++ android/app/src/main/java/com/ttnconsole/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
-import io.sentry.RNSentryPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
