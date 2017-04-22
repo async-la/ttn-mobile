3c036f4382370825d1f69b2692cbb0d91ba254d0
Zack Story <zack@root-two.com>
2017-04-22 15:16:58 -0700

    [types] more general type fixes (#97)

--- src/i18n/LanguageProvider.js
+++ src/i18n/LanguageProvider.js
@@ -7,8 +7,9 @@ import { DEFAULT_LOCALE } from './'
 
 type Props = {
   messages: { [key: string]: string },
-  children: React.Element<any>,
-};
+  children?: React.Element<any>,
+  translations: Object, // @TODO better intl typing
+}
 
 // @TODO: figure out locale or set it somewhere [cc]
 
