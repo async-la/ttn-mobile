83eb94300572f91d80f980d9e5360a80d75414ab
rt2zz <zack@root-two.com>
2017-04-22 13:52:11 -0700

    [types] more tricky thunk typing

--- src/types/redux.js
+++ src/types/redux.js
@@ -4,8 +4,5 @@ import type { State } from '../scopes/rootReducer'
 import type { Action } from '../scopes/types'
 
 export type GetState = () => State
-type Thunk = (
-  Function,
-  GetState
-) => Action | Promise<Action> | void | Promise<void>
+type Thunk = (Function, GetState) => Action | Promise<any> | void
 export type Dispatch = (Action | Thunk) => Action | Promise<Action>
