b8db7a2f6050c7a7c34138f93d64b1ea73e539c7
Christopher Dro <hello@blick-labs.com>
2017-05-01 15:03:45 -0700

    [DataMonitor] Improve connection status monitoring

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,19 +1,40 @@
 // @flow
 
 import React from 'react'
-import { Platform, TouchableOpacity } from 'react-native'
+import { ActivityIndicator, Platform, TouchableOpacity } from 'react-native'
 
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import { BLUE, WHITE } from '../constants/colors'
+import { BLUE, GREY, WHITE } from '../constants/colors'
 
-const ClearButton = (props: Object) => {
+const ClearButton = ({
+  disabled,
+  header,
+  inProgress,
+  shadow = true,
+  onPress,
+}: Object) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={props.onPress}>
-      <Ionicons
-        name="ios-trash-outline"
-        size={25}
-        style={{ color: WHITE, backgroundColor: 'transparent' }}
-      />
+    <TouchableOpacity
+      style={[
+        styles.button,
+        shadow && styles.shadow,
+        header && styles.isHeader,
+        disabled && styles.inactive,
+        header && shadow && styles.headerShadow,
+      ]}
+      onPress={onPress}
+    >
+      {inProgress
+        ? <ActivityIndicator color={BLUE} />
+        : <Ionicons
+            name="ios-refresh"
+            size={header ? 25 : 35}
+            style={[
+              styles.icon,
+              header && styles.iconHeader,
+              disabled && styles.inactiveIcon,
+            ]}
+          />}
     </TouchableOpacity>
   )
 }
@@ -32,5 +53,49 @@ const styles = {
     borderRadius: 44,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  inactive: {
+    backgroundColor: GREY,
+    borderColor: GREY,
+  },
+  inactiveIcon: {
+    color: WHITE,
+  },
+  isHeader: {
+    position: 'relative',
+    bottom: undefined,
+    right: undefined,
+    width: 25,
+    height: 25,
+    backgroundColor: WHITE,
+    borderColor: BLUE,
+    borderWidth: 1,
+  },
+  icon: {
+    color: WHITE,
+    backgroundColor: 'transparent',
+  },
+  iconHeader: {
+    color: BLUE,
+  },
+  shadow: {
+    elevation: 5,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  headerShadow: {
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
   },
 }
