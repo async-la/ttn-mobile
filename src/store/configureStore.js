a9c0113242ee8ef3291b9ec86584a50506818f20
Christopher Dro <hello@blick-labs.com>
2017-05-12 10:46:19 -0700

    [Store] Purge on crash Closes #146

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,7 @@
 // @flow
 
+declare var ErrorUtils: any
+
 import { AsyncStorage } from 'react-native'
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
@@ -21,6 +23,15 @@ export default () => {
   const store = createStore(stateManagedReducer, undefined, enhancers)
   const persistor = persistStore(store, { storage: AsyncStorage })
   //.purge()
+
+  if (process.env.NODE_ENV === 'production') {
+    let originalGlobalErrorHandler = ErrorUtils.getGlobalHandler()
+    ErrorUtils.setGlobalHandler(async error => {
+      await persistor.purge()
+      ErrorUtils.setGlobalHandler(originalGlobalErrorHandler)
+      throw error
+    })
+  }
 
   return { store, persistor }
 }
